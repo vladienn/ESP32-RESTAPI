dd5609c449cb356f0f120f70b71275409a018d04
Vladislav Enev <52584513+vladienn@users.noreply.github.com>
2021-01-30 01:27:12 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import json
+app = Flask(__name__)
+
+data = {'light_state':'off'}
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    global data
+    if request.method == 'GET':
+        print(data)
+        return jsonify(data)
+    if request.method == 'POST':
+        #json_data = request.get_json()
+        data_req = request.data
+        json_data = json.loads(data_req)
+        print(json_data)
+        data = json_data.copy()
+        return jsonify(data)
+
+    
+
+
+app.run('0.0.0.0', port = 5001)
